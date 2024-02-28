be77545b2d8632c5108ff5e80ea8b8272a456c56
Humayun Kabir <pythonloverkabir11@gmail.com>
2024-02-28 11:01:53 +0600

    new

---
+++ lib/main.dart
@@ -0,0 +1,150 @@
+
+
+
+
+
+import 'dart:async';
+import 'dart:io';
+
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:flutter_pdfview/flutter_pdfview.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  String pathPDF = "";
+
+  @override
+  void initState() {
+    super.initState();
+    fromAsset('assets/documents/file-sample_150kB.pdf', 'sample.pdf').then((f) {
+      setState(() {
+        pathPDF = f.path;
+      });
+    });
+  }
+
+  Future<File> fromAsset(String asset, String filename) async {
+    Completer<File> completer = Completer();
+    try {
+      var dir = await getApplicationDocumentsDirectory();
+      File file = File("${dir.path}/$filename");
+      var data = await rootBundle.load(asset);
+      var bytes = data.buffer.asUint8List();
+      await file.writeAsBytes(bytes, flush: true);
+      completer.complete(file);
+    } catch (e) {
+      throw Exception('Error parsing asset file!');
+    }
+    return completer.future;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final themeData = Theme.of(context);
+    return MaterialApp(
+      home: Scaffold(body: Builder(
+        builder: (BuildContext context) {
+          return Center(
+              child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    ElevatedButton(
+                      child: Text('Tap to Open Document',
+                          style: themeData.textTheme.headline4
+                              ?.copyWith(fontSize: 21.0)),
+                      onPressed: () {
+                        if (pathPDF.isNotEmpty) {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => PDFScreen(path: pathPDF),
+                            ),
+                          );
+                        }
+                      },
+                    )
+                  ]));
+        },
+      )),
+    );
+  }
+}
+
+class PDFScreen extends StatefulWidget {
+  final String? path;
+
+  const PDFScreen({Key? key, this.path}) : super(key: key);
+
+  @override
+  _PDFScreenState createState() => _PDFScreenState();
+}
+
+class _PDFScreenState extends State<PDFScreen> with WidgetsBindingObserver {
+  final Completer<PDFViewController> _controller =
+  Completer<PDFViewController>();
+  int? pages = 0;
+  int? currentPage = 0;
+  bool isReady = false;
+  String errorMessage = '';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Document"),
+        actions: <Widget>[
+          IconButton(
+            icon: const Icon(Icons.share),
+            onPressed: () {},
+          ),
+        ],
+      ),
+      body: Stack(
+        children: <Widget>[
+          PDFView(
+            filePath: widget.path,
+            onRender: (_pages) {
+              setState(() {
+                pages = _pages;
+                isReady = true;
+              });
+            },
+            onError: (error) {
+              setState(() {
+                errorMessage = error.toString();
+              });
+              print(error.toString());
+            },
+            onPageError: (page, error) {
+              setState(() {
+                errorMessage = '$page: ${error.toString()}';
+              });
+              print('$page: ${error.toString()}');
+            },
+            onViewCreated: (PDFViewController pdfViewController) {
+              _controller.complete(pdfViewController);
+            },
+          ),
+          errorMessage.isEmpty
+              ? !isReady
+              ? const Center(
+            child: CircularProgressIndicator(),
+          )
+              : Container()
+              : Center(
+            child: Text(errorMessage),
+          )
+        ],
+      ),
+    );
+  }
+}
